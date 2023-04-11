2b70b2651236becf0ae16782802b5a4c58e007cf
BellaLaptop <arabela_tso@outlook.com>
2023-04-11 18:44:32 +0800

    update eval script

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,7 +1,6 @@
 python eval.py \
   --device 0,1 \
-  --length 900 \
   --model_config model/final_model/config.json \
   --tokenizer_path cache/vocab.txt \
-  --model_path model/final_model \
+  --pretrained_model model/final_model \
   --stride 4 \
