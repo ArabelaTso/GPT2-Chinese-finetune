1792c81f6a3dcb7bcf825d45559fc9cc6dfb6933
BellaLaptop <arabela_tso@outlook.com>
2023-04-11 19:20:29 +0800

    add preprocess

---
+++ data/preprocess.py
@@ -0,0 +1,11 @@
+
+if __name__ == '__main__':
+    in_file = './test_1500.txt'
+    out_file = './test_1500_try.json'
+    
+    lines = open(in_file, 'r').readlines()
+    
+    print(f'Read {len(lines)} lines.')
+    with open(out_file, 'w') as fw:
+        fw.write('[\"' + '\n'.join([line.replace("\"", "", line) for line in lines] + '\"]'))
+    
