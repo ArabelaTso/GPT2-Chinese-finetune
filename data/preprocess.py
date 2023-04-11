70050050f40b98c9e08daedc02a759b5cf3ed9ac
BellaLaptop <arabela_tso@outlook.com>
2023-04-11 19:32:36 +0800

    update

--- data/preprocess.py
+++ data/preprocess.py
@@ -6,14 +6,11 @@ if __name__ == '__main__':
     lines = open(in_file, 'r').readlines()
     
     print(f'Read {len(lines)} lines.')
-    print(lines)
     
     with open(out_file, 'w') as fw:
         fw.write("[\"")
         for line in lines:
             line = line.replace("\"", "")
-            fw.write(line + '\n')
+            fw.write(line)
         fw.write("\"]")
-        # fw.write("[\"{}\"]".format(''.join([line.replace("\"", "", line) for line in lines])))
-        # fw.write('[\"' + '\n'.join([line.replace("\"", "", line) for line in lines] + '\"]'))
     
