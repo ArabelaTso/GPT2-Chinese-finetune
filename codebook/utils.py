7eccdee00b3c4fa8f226554dcce01a9023ea6726
BellaLaptop <arabela_tso@outlook.com>
2023-04-19 22:57:54 +0800

    add more preprocess

--- codebook/utils.py
+++ codebook/utils.py
@@ -2,11 +2,16 @@ import re
 
 def preprocess(line):
     line = line.rstrip()
+    line = line.lstrip()
+    line = line.replace("\"", "")
+    line = line.replace('嘱：', '')
+    if "局麻麻木感通常3个小时" in line:
+        print(str(line))
+    line = re.sub(r'^\d[，.]\s?', '', line)
     line = re.sub(r"Frankl \d-\d\s?，?", '', line)
     line = re.sub(r"Frankl \d\s?，?", '', line)
     line = line.replace('\t', '')
     line = line.replace('\\', ' ')
-    line = line.replace("\"", "")
     line = line.replace(' ', '')
 
     return line
