575890ddbcd3fb0cbd9d3382d1750c0f01dce30c
BellaLaptop <arabela_tso@outlook.com>
2023-05-09 10:37:04 +0800

    add a way to output all the sentences

--- codebook/analysis.py
+++ codebook/analysis.py
@@ -4,7 +4,7 @@ from utils import preprocess
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', default='./data/test_1500.txt', type=str, required=False, help='Input file path in txt format.')
+    parser.add_argument('--input', default='./data/train_all.txt', type=str, required=False, help='Input file path in txt format.')
     # parser.add_argument('--output', default='../data/test_1500.json', type=str, required=False,
                         # help='Output file in json format.')
     
@@ -13,11 +13,15 @@ if __name__ == '__main__':
     in_file = args.input
     # out_file = args.output
     
+    thr_cnt = 1
+    thr_len = 1
+    
     lines = open(in_file, 'r').readlines()
     
     print(f'Read {len(lines)} lines.')
     
-    sents = defaultdict(int)
+    sent_count_dict = defaultdict(int)
+    sent_all = []
     
     for line in lines:
         line = preprocess(line)
@@ -26,12 +30,20 @@ if __name__ == '__main__':
         
         for sent in line.split('。'):
             if len(sent):
-                sents[sent] += 1
+                sent_count_dict[sent] += 1
+                sent_all.append(sent)
     
-    sorted_sents = sorted(sents.items(), key=lambda x:x[1], reverse=True)
-    for st, v in sorted_sents:
-        # if "4" == st:
-            # print(str(st))
+    # Output statistics, a sentence followed by its counts
+    # Sort dictionary by descending counts
+    sorted_sent_count_dict = sorted(sent_count_dict.items(), key=lambda x:x[1], reverse=True)
+    for st, v in sorted_sent_count_dict:
+        if len(st) <= 1:
+            continue
         if v > 5:
-            print(st, v)
+            print(st)
+            # print(st, v)
     
+    # Output all sentence, without removing the duplication
+    for sent in sent_all:
+        if len(sent) > 1:
+            print(sent)
