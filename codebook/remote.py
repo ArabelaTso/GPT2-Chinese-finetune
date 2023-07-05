53cc51f26d35cf5d24fed8297ceba36383941852
ArabelaTso <arabela_tso@outlook.com>
2023-07-05 16:43:29 +0800

    update two types of output

--- codebook/remote.py
+++ codebook/remote.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request
 
-from prefix_inferring import setup
+from combine import setup_models, get_replies_from_two_models
 
-trie = setup()
+pre_pt_filename = './models/prefix.json'
+cont_pt_filename = './models/contain.json'
+    
+prefix_model, contain_model = setup_models(pre_pt_filename, cont_pt_filename)
+
 
 def get_reply(prompt):
-    results = trie.search(prompt)
-    # print(results)
+    results = get_replies_from_two_models(prefix_model, contain_model, prompt=prompt)
+    print(results)
     if len(results) == 0:
-        result = 'Not found.'
-    else:
-        result = results[0][0]
-    return result
+        results = ['Not found.']
+    return results
 
 app = Flask(__name__)
 
@@ -22,11 +24,13 @@ def home():
 @app.route('/', methods=['POST'])
 def process_input():
     input_text = request.form['input']
-    output_text = get_reply(input_text) #'Hello ' + input_text
-    return render_template('index.html', input=input_text, output=output_text)
+    # return a string
+    output_text = get_reply(input_text)
+    
+    return render_template('index3.html', input=input_text, output_list=output_text)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8090)
+    app.run(host='localhost', port=8091)
     
     # app.run(host='localhost', port=9999)
     # app.run(host='10.79.219.111', port=5000)
